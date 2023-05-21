54e54939614c30fca4f43c5c475e064416920350
Alexander Borouhin <a@borouhin.com>
2023-05-22 00:18:59 +0300

    Add project files.

---
+++ LineProtocolLine.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zfs_collector;
+internal class LineProtocolLine
+{
+    private string _source_line;
+    private List<string>? _fields;
+    private List<string>? _tags;
+    private string? _measurement;
+    private long? _timestamp;
+    internal LineProtocolLine(string line)
+    {
+        _source_line = line;
+    }
+    internal List<string> PrometheusLines(bool process_timestamps = false)
+    {
+        List<string> strings = new();
+        try
+        {
+            _measurement = _source_line.Split(',')[0];
+            _tags = _source_line.Split(' ')[0].Split(',').Skip(1).ToList();
+            _fields = _source_line.Split(' ')[1].Split(',').ToList();
+        }
+        catch
+        {
+            System.Console.Error.WriteLine($"Error parsing line: {_source_line}, skipping.");
+            return strings;
+        }
+        _timestamp = null;
+        if (process_timestamps)
+        {
+            try
+            {
+                _timestamp = Convert.ToInt64(_source_line.Split(' ')[2]) / 1000;
+            }
+            catch
+            {
+                System.Console.Error.WriteLine($"Error parsing timestamp for {_measurement} : \"{_timestamp.ToString}\", skipping.");
+                _timestamp = null;
+            }
+        }
+
+        foreach (string field in _fields)
+        {
+            string tagstring = "";
+            string fieldname, fieldvalue, timestampstr;
+            try
+            {
+                fieldname = field.Split('=')[0];
+                fieldvalue = field.Split('=')[1].TrimEnd('u');
+                timestampstr = (_timestamp != null) ? " " + _timestamp.ToString() : "";
+            }
+            catch
+            {
+                System.Console.Error.WriteLine($"Error parsing field for {_measurement} : \"{field}\", skipping.");
+                continue;
+            }
+            foreach (string tag in _tags)
+            {
+                try
+                {
+                    tagstring += $"{tag.Split('=')[0]}=\"{tag.Split('=')[1]}\",";
+
+                }
+                catch
+                {
+                    System.Console.Error.WriteLine ($"Error parsing tag for {_measurement}_{field} : \"{tag}\", skipping.");
+                }
+            }
+            strings.Add($"{_measurement}_{fieldname}{{{tagstring.TrimEnd(',')}}} {fieldvalue}{timestampstr}");
+        }
+        return strings;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace zfs_collector;
+
+internal class Program
+{
+    private static string _zpool_path = "zpool_influxdb";
+    private static int _timeout = 10000;
+
+    public static int Main(string[] args)
+    {
+        Process process = new()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = _zpool_path,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                RedirectStandardError = true
+            }
+        };
+
+        StringBuilder output = new();
+        StringBuilder error = new();
+        using (AutoResetEvent outputWaitHandle = new(false))
+        using (AutoResetEvent errorWaitHandle = new(false))
+        {
+            process.OutputDataReceived += (sender, e) => {
+                if (e.Data == null)
+                {
+                    outputWaitHandle.Set();
+                }
+                else
+                {
+                    output.AppendLine(e.Data);
+                }
+            };
+            process.ErrorDataReceived += (sender, e) =>
+            {
+                if (e.Data == null)
+                {
+                    errorWaitHandle.Set();
+                }
+                else
+                {
+                    error.AppendLine(e.Data);
+                }
+            };
+
+            try
+            {
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                System.Console.Error.WriteLine("Failed to start " + _zpool_path + " : " + ex.Message);
+                return 1;
+            }
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            if (process.WaitForExit(_timeout) &&
+                outputWaitHandle.WaitOne(_timeout) &&
+                errorWaitHandle.WaitOne(_timeout))
+            {
+                foreach (string line in output.ToString().Split('\n'))
+                {
+                    if (line != "")
+                    {
+                        LineProtocolLine lpl = new(line);
+                        foreach (string promline in lpl.PrometheusLines())
+                        {
+                            System.Console.WriteLine(promline);
+                        }
+                    }
+                }
+                System.Console.Error.WriteLine(error);
+                return 0;
+            }
+            else
+            {
+                System.Console.Error.WriteLine("{0} timed out", _zpool_path);
+                return 1;
+            }
+        }
+    }
+}
